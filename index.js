1ed6b36befd47fbcb9326fc0d69f4a4f7689f31f
mmmerili <meriliru@metropolia.fi>
2021-03-30 23:37:04 +0300

    grouping routes

--- index.js
+++ index.js
@@ -2,28 +2,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const catRouter = require('./routes/catRoute')
 
 app.use(express.static('public'));
 
-app.get('/cat', (req, res) => {
-  console.log('get cat');
-  res.send('Hello cat!')
+app.get("/", (req,res) => {
+  res.send('Hello World!');
 });
 
-app.post('/cat', (req, res) => {
-console.log('post cat');
-res.send('post cat');
-});
 
-app.put('/cat', (req, res) => {
-  console.log('put cat');
-  res.send('put cat');
-});
-
-  app.delete('/cat', (req, res) => {
-    console.log('delete cat');
-    res.send('delete cat');
-});
-
+app.use('/cat', catRouter);
 
 app.listen(port, () => console.log(`Example app listening at http://localhost${port}!`));
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -2,6 +2,32 @@
 const express = require('express');
 var router = express.Router();
 
+router.route('/')
+.get((req, res) => {
+    console.log('get all cats');
+    res.send('Hello cat!')
+  })
+  
+.post((req, res) => {
+  console.log('post cat');
+  res.send('post cat');
+  });
+  
+
+router.route('/:id')
+    .get((req, res) => {
+        console.log('get one cat by id' , req.params );
+        res.send(`Hello cat with id ${req.params.id}!`)
+      })
+    .put((req, res) => {
+    console.log('put cat' , req.params);
+    res.send('put cat');
+  })
+    .delete((req, res) => {
+      console.log('delete cat' , req.params);
+      res.send('delete cat');
+  });
+
 module.exports = router;
 
 // catRoute
