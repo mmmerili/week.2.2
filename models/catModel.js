6ea3a4c0b7cd3187dcf28915b9334c4a50b16e51
mmmerili <meriliru@metropolia.fi>
2021-03-31 11:53:30 +0300

    database etc

--- models/catModel.js
+++ models/catModel.js
@@ -1,23 +1,31 @@
 'use strict';
-const cats = [
-  {
-    id: '1',
-    name: 'Frank',
-    age: '6',
-    weight: '5',
-    owner: '1',
-    filename: 'http://placekitten.com/400/300',
-  },
-  {
-    id: '2',
-    name: 'James',
-    age: '4',
-    weight: '11',
-    owner: '2',
-    filename: 'http://placekitten.com/400/302',
-  },
-];
+
+const pool = require('../database/db');
+const promisePool = pool.promise();
+
+const getAllCats = async () => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner name too.
+    const [rows] = await promisePool.query('SELECT * FROM wop_cat');
+    console.log('something back from db?', rows);
+    return rows;
+  } catch (e) {
+    console.error('error', e.message);
+  }
+};
+
+const getAllCatsSort = async (order) => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner name too.
+    const [rows] = await promisePool.query('SELECT * FROM wop_cat ORDER BY ...');
+    return rows;
+  } catch (e) {
+    console.error('error', e.message);
+  }
+};
+
 
 module.exports = {
-  cats,
+  getAllCats,
+  getAllCatsSort,
 };
--- models/controllers/catController.js
+++ models/controllers/catController.js
@@ -4,13 +4,15 @@ const catModel = require('../catModel');
 
 const cats = catModel.cats;
 
-const cat_list_get = (req, res) => {
+const cat_list_get = async (req, res) => {
     console.log('get all cats from controllers' , req.query);
     if(req.query.sort == 'age'){
-        const catsSort = cats.slice().sort((catA, catB) => catA.age - catB.age);
-        res.json(catsSort);
+        //const catsSort = cats.slice().sort((catA, catB) => catA.age - catB.age);
+        res.json({todo: 'will do later'});
         return;
     }
+    
+  const cats = await catModel.getAllCats();
   res.json(cats);
 };
 
